32ad00a543e5f118ce29b56878c6cafaf98e2f98
isaquelimaof <isaquelimaof@gmail.com>
2022-08-14 18:02:26 -0300

    Implementando telaInicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageLoginComponents } from './pages/pageLogin/pageLogin.component';
+import { TelaInicialComponent } from './pages/telaInicial/telaInicial.component';
 
 const routes: Routes = [
+  {path: '',component: TelaInicialComponent},
+  {path: 'telaInicial',component: TelaInicialComponent},
   {path: 'pageLogin', component: PageLoginComponents}
 ];
 
---
+++ src/app/pages/telaInicial/telaInicial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'telaInicial',
+  templateUrl: './telaInicial.component.html',
+  styleUrls: ['./telaInicial.component.css']
+})
+
+export class TelaInicialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
